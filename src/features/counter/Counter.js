1da86b130119784eec8dbd9c81c9410724f8bca8
AbdelMounhim <abunawfal@gmail.com>
2021-05-19 18:31:25 +0200

    test on connected components

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
 import {
   decrement,
   increment,
@@ -7,28 +7,31 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from "./counterSlice"
+import styles from "./Counter.module.css"
 
-export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
-
-  const incrementValue = Number(incrementAmount) || 0;
-
+export default function Counter() {
+  const count = useSelector(selectCount)
+  const dispatch = useDispatch()
+  const [incrementAmount, setIncrementAmount] = useState("2")
+  const incrementValue = Number(incrementAmount) || 0
   return (
     <div>
       <div className={styles.row}>
         <button
+          type="button"
           className={styles.button}
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
-        <span className={styles.value}>{count}</span>
+        <span data-testid="counter" className={styles.value}>
+          {count}
+        </span>
         <button
+          type="button"
+          data-testid="incrementButton"
           className={styles.button}
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
@@ -38,24 +41,29 @@ export function Counter() {
       </div>
       <div className={styles.row}>
         <input
+          data-testid="incrementAmount"
           className={styles.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
+          type="button"
+          data-testid="incrementByAmount"
           className={styles.button}
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
         </button>
         <button
+          type="button"
           className={styles.asyncButton}
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
         </button>
         <button
+          type="button"
           className={styles.button}
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
@@ -63,5 +71,5 @@ export function Counter() {
         </button>
       </div>
     </div>
-  );
+  )
 }
---
+++ src/features/counter/Counter.test.js
@@ -0,0 +1,68 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from "react"
+import { Provider } from "react-redux"
+import { combineReducers, createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import { render, fireEvent, cleanup } from "@testing-library/react"
+import Counter from "./Counter"
+import reducer, {
+  selectCount,
+  increment,
+  decrement,
+  incrementByAmount,
+  incrementAsync,
+} from "./counterSlice"
+
+let store
+
+const createTestStore = () =>
+  createStore(combineReducers({ counter: reducer }), applyMiddleware(thunk))
+
+const renderWithRedux = (component) =>
+  render(<Provider store={store}>{component}</Provider>)
+
+beforeEach(() => {
+  store = createTestStore()
+})
+afterEach(cleanup)
+
+it("Counter Component / initial State ", () => {
+  const { getByTestId } = renderWithRedux(<Counter />)
+  const initialState = {
+    counter: {
+      value: 0,
+      status: "idle",
+    },
+  }
+  expect(getByTestId("counter")).toHaveTextContent("0")
+  expect(store.getState()).toEqual(initialState)
+  expect(selectCount(store.getState())).toBe(0)
+})
+
+it("Counter component  / basic actions ", () => {
+  const { getByTestId } = renderWithRedux(<Counter />)
+  store.dispatch(increment())
+  store.dispatch(decrement())
+  store.dispatch(incrementByAmount(5))
+  expect(getByTestId("counter")).toHaveTextContent("5")
+})
+
+it(" Counter component / incrementAsync", () => {
+  const { getByTestId } = renderWithRedux(<Counter />)
+  store.dispatch(incrementAsync(5)).then(() => {
+    expect(getByTestId("counter")).toHaveTextContent("10")
+  })
+})
+
+it(" Counter component / click increment button ", () => {
+  const { getByTestId } = renderWithRedux(<Counter />)
+  fireEvent.click(getByTestId("incrementButton"))
+  expect(getByTestId("counter")).toHaveTextContent("1")
+})
+
+it(" Counter component / change increment amount value ", () => {
+  const { getByTestId } = renderWithRedux(<Counter />)
+  fireEvent.change(getByTestId("incrementAmount"), { target: { value: 4 } })
+  fireEvent.click(getByTestId("incrementByAmount"))
+  expect(getByTestId("counter")).toHaveTextContent("4")
+})
